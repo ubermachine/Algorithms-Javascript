f734c78284c4632c423a64a1a6b99b5a09cb437b
ubermachine <shivamkatoch@outlook.com>
2020-08-03 19:17:43 +0530

    codeforce?

--- week3_greedy_algorithms/3_car_fueling/diffip.js
+++ week3_greedy_algorithms/3_car_fueling/diffip.js
@@ -7,23 +7,56 @@ const rl = readline.createInterface({
 });
 
 process.stdin.setEncoding('utf8');
-var arr=[]
+let arr=[]
 rl.on('line', (input) => {
+    
 arr.push(input)
   
 }).on('close', () => {
-    knap(arr)
+    console.log(minfill(arr))
     process.exit(0);
     
 });
-function knap(arr){
-    let n=arr[0].split(" ")[0]
-    let weight=arr[0].split(" ")[1]
-    console.log(n,weight)
-    return (arr)
+function minfill(arr){
+    let d=arr[0],mileage=arr[1],n=arr[2];
+    dset=arr[3].split(' ')//.map(a=>Number(a))
+
+    let fills=0
+    let dt=0
+    let i=0   
+    let meter=dset[i]
+    while(dt<d&&i<n){
+        let kmleft=mileage
+        while(kmleft){
+            if(Math.abs(dt-meter)+kmleft>=d){
+                console.log('ho',fills,dt,meter)
+                break
+            }
+            if(kmleft<Math.abs(dt-meter)){
+                console.log('ho over',fills,dt,meter)
+                return -1
+                break //just added for assurity
+            }else{
+                  if(kmleft>Math.abs(dt-meter)&&i<n-1){
+                      meter=dset[++i] 
+                      console.log(i,n)
+                  }
+                  
+                console.log('filleed',fills,dt,meter)
+                dt=meter
+                if(dt+mileage<=d){fills++}
+                meter=dset[++i]
+                 kmleft=0
+            }
+        }
+        
+        
+    }
+    
+    return fills
     
 }
 
 
-
+module.exports=(minfill)
 
