0f67fa560e65cb5e2e8bcdd904dcaec3a0dc36ee
ubermachine <shivamkatoch@outlook.com>
2020-08-06 12:11:33 +0530

    week4 majority

---
+++ week4_divide_and_conquer/2_majority_element/Makorityeasy.js
@@ -0,0 +1,39 @@
+ /*
+Own input stream*/
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    terminal: false
+});
+
+process.stdin.setEncoding('utf8');
+let arr=[]
+rl.on('line', (input) => {
+    
+arr.push(input)
+  
+}).on('close', () => {
+    console.log(maxfill(arr))
+    process.exit(0);
+    
+});
+function maxfill(arr){
+   let brr={}
+   let n= arr[0],nums=arr[1].split(" ")
+   let c=1,max=0,digit=0
+   for(let i of nums){
+       if(!brr[i]){
+           brr[i]=c
+       }else{
+           brr[i]+=c
+       if(brr[i]>max)max=brr[i]
+           
+       }
+   }
+   if(max>n/2)return 1
+   return 0
+}
+
+module.exports=(maxfill)
+
+ 
